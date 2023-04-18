b4b87fed96cc58d5e66404634b473629058ec1e8
Simon <simon.dieny@gmail.com>
2023-04-18 15:02:39 +0200

    standalone API : remove AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { InMemoryDataService } from './in-memory-data.service';
 import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', loadComponent: () => import('./login/login.component').then(module => module.LoginComponent) },
+    { path: '**', loadComponent: () => import('./page-not-found/page-not-found.component').then(module => module.PageNotFoundComponent) }
+  ];
 
 @NgModule({
     declarations: [AppComponent],
@@ -19,7 +25,7 @@ import { LoginComponent } from './login/login.component';
         HttpClientModule,
         HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
         PokemonModule,
-        AppRoutingModule,
+        RouterModule.forRoot(routes),
         PageNotFoundComponent,
         LoginComponent
     ],
