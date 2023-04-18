672210c0b35e1b4d293f719aff522799eb7e3a29
Simon <simon.dieny@gmail.com>
2023-04-18 14:32:52 +0200

    standalone API : load standalone component login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', loadComponent: () => import('./login/login.component').then(module => module.LoginComponent) },
   { path: '**', component: PageNotFoundComponent }
 ];
 
