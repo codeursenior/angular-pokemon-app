6157a58ff7b9e222ae40770cdf4e8560bfc947ad
Simon <simon.dieny@gmail.com>
2023-04-18 15:33:13 +0200

    remove unused importation in pokemon routes

--- src/app/pokemon/pokemon.routes.ts
+++ src/app/pokemon/pokemon.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from "@angular/router";
-import { EditPokemonComponent } from "./edit-pokemon/edit-pokemon.component";
-import { AddPokemonComponent } from "./add-pokemon/add-pokemon.component";
-import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
-import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
 import { AuthGuard } from "../auth.guard";
 
 export const pokemonRoutes: Routes = [
