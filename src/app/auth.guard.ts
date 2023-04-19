aa82c429e37d68f1765e9a14941b5a793e6624cb
Simon <simon.dieny@gmail.com>
2023-04-19 09:38:32 +0200

    functional guard

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,23 +1,15 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
-
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
-
-  canActivate(): boolean {
-    if(this.authService.isLoggedIn) {
-      return true;
-    }
-
-    this.router.navigate(['/login']);
-    return false;
+export const AuthGuard = () => {
+  const router = inject(Router);
+  const authService = inject(AuthService)
+  
+  if(authService.isLoggedIn) {
+    return true;
   }
+
+  router.navigate(['/login']);
+  return false;  
 }
